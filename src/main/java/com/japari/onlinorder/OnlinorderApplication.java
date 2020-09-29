5961f9f758649db667361b28c708e4b5bab9ff54
ke <yooork99@gmail.com>
2020-09-29 11:52:53 -0400

    first commit

---
+++ src/main/java/com/japari/onlinorder/OnlinorderApplication.java
@@ -0,0 +1,13 @@
+package com.japari.onlinorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/japari/onlinorder/OnlinorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.japari.onlinorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
